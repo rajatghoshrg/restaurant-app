0d9c28fb9c3e775cc648c9198e21ad7db6f1e98a
Rajat Ghosh <rajatghoshhh@gmail.com>
2026-04-13 12:28:38 +0530

    removed unused part

--- src/component/basics/resturant.js
+++ src/component/basics/resturant.js
@@ -4,16 +4,17 @@ import MenuCard from './MenuCard.js';
 import Navbar from './Navbar.js';
 
 const Resturant = () => {
-    
+
    const uniqueList = [
     "All",
     ...new Set(
         Menu.map((curElem) => curElem.category)
     ),
-];
+   ];
 
     const [menuData, setMenuData] = useState(Menu);
-    const [menuList, setMenuList] = useState(uniqueList);
+    const [menuList] = useState(uniqueList); // ✅ FIXED HERE
+
     const filterItem = (category) => {
 
         if (category === "All") {
@@ -24,13 +25,14 @@ const Resturant = () => {
         const updatedList = Menu.filter((curElem) => {
             return curElem.category === category;
         });
+
         setMenuData(updatedList);
     };
 
     return (
         <>
             <div className="restaurant-header">
-                <h1> Pet Pujo Adda</h1>
+                <h1>Pet Pujo Adda</h1>
                 <p>Eat • Chill • Adda Repeat</p>
             </div>
             <Navbar filterItem={filterItem} menulist={menuList} />
@@ -39,4 +41,4 @@ const Resturant = () => {
     )
 }
 
-export default Resturant
+export default Resturant;
